722d91283d2d3dda55f1bd3f5929b75ca6dbcb7e
Dane Hillard <github@danehillard.com>
2016-02-03 16:42:14 -0500

    Make import structure a bit cleaner

---
+++ enigma/__init__.py
@@ -0,0 +1,3 @@
+from enigma.enigma import Enigma
+
+__all__ = ['Enigma']
--- tests/test_enigma.py
+++ tests/test_enigma.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 from unittest.mock import patch
 
-from enigma.enigma import Enigma
+from enigma import Enigma
 
 
 class SingleLetterCipherTestCase(TestCase):
