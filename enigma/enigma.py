581a2578e721c729116f9b45ca755587247b7bd3
Elliot Charney <elliot.charney@ithaka.org>
2017-04-28 22:29:30 -0400

    extend refactor to make initial tests use a default reflector

--- enigma/enigma.py
+++ enigma/enigma.py
@@ -1,26 +1,14 @@
 from string import ascii_uppercase
 
 class Enigma(object):
-    def __init__(self, reflector=None):
+    def __init__(self, reflector='EJM'):
         self.reflector = reflector
         self.reflector_is_enabled = False
 
     def encrypt(self, message):
-        if self.reflector:
-            encrypted_string = ''
-            for letter in message:
-                encrypted_string += self.reflector[self.get_index(letter)]
-            return encrypted_string
-
         encrypted_string = ''
         for letter in message:
-            if letter == 'A':
-                encrypted_string += 'E'
-            if letter == 'B':
-                encrypted_string += 'J'
-            if letter == 'C':
-                encrypted_string += 'M'
-
+            encrypted_string += self.reflector[self.get_index(letter)]
         return encrypted_string
 
     def enable_reflector(self):
